4628c334a3567061d56a23f329b7e9654dc59694
KristianWold <kriswold@student.matnat.uio.no>
2019-11-21 22:57:00 +0100

    Start solving ODE with DNN

--- src/autograd_test.py
+++ src/autograd_test.py
@@ -4,6 +4,7 @@ from autograd import elementwise_grad
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from sklearn import datasets
 
 
 def feedforward(x, P, act_func):
@@ -17,46 +18,57 @@ def feedforward(x, P, act_func):
     return z
 
 
-def cost(x, P, act_func, y):
-    y_pred = feedforward(x, P, act_func)
-    return np.mean((y_pred - y)**2)
+def g_trial(x, P, act_func):
+    return x * (x - 6) * feedforward(x, P, act_func)
+
+
+ddg_trial = elementwise_grad(elementwise_grad(g_trial, 0), 0)
+
+
+def cost(x, P, act_func):
+    RHS = ddg_trial(x, P, act_func)
+    LHS = -g_trial(x, P, act_func)
+    return np.mean((RHS - LHS)**2)
+
+
+def generate_P(dim):
+    n = len(dim) - 1
+    P = n * [None]
+    for i in range(n):
+        P[i] = np.random.normal(0, 1, (dim[i] + 1, dim[i + 1]))
+        P[i][-1] = 0.01 * np.ones(dim[i + 1])
+
+    return P
 
 
 np.random.seed(42)
-
-
-def y_func(x):
-    return np.cos(x)
-
-
 x = np.array([[i] for i in np.linspace(0, 2 * np.pi, 1000)])
 
 
-y = y_func(x)
-
-P = 2 * [None]
-
-P[0] = np.random.normal(0, 1, (2, 20))
-P[0][-1] = 0.01 * np.ones(20)
-
-print(P[0])
-
-P[1] = np.random.normal(0, 1, (21, 1))
-P[1][-1] = 0.01 * np.ones(1)
-
-
+P = generate_P((1, 40, 1))
 act_func = [np.tanh, lambda x: x]
-
-feedforward(x, P, act_func)
 
 grad = elementwise_grad(cost, 1)
 
-for i in range(10000):
-    print(cost(x, P, act_func, y))
-    gradient = grad(x[idx], P, act_func, y[idx])
-    P[0] -= 0.01 * gradient[0]
-    P[1] -= 0.01 * gradient[1]
+N = 10000
 
-plt.plot(x, feedforward(x, P, act_func))
-plt.plot(x, y)
+a = 0
+
+g1 = g2 = g3 = 0
+
+for i in range(N):
+    #print(cost(x, P, act_func, y))
+    if i % (N / 100) == 0:
+        print(i / (N / 100))
+
+    gradient = grad(x, P, act_func)
+    g1 = gradient[0] + a * g1
+    g2 = gradient[1] + a * g2
+
+    P[0] -= 0.001 * g1
+    P[1] -= 0.001 * g2
+
+print(cost(x, P, act_func))
+
+plt.plot(x, g_trial(x, P, act_func))
 plt.show()
