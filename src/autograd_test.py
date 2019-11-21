c037b088a74cee05bffa76f990ae4008b1137b84
KristianWold <kriswold@student.matnat.uio.no>
2019-11-21 01:29:01 +0100

    Implementing NN using autograd

--- src/autograd_test.py
+++ src/autograd_test.py
@@ -9,9 +9,10 @@ from matplotlib import cm
 def feedforward(x, P, act_func):
     dim = len(P)
     z = x
+
     for i in range(dim):
         z = np.concatenate((z, np.ones((z.shape[0], 1))), axis=1)
-        z = np.tanh(z @ P[i])
+        z = act_func[i](z @ P[i])
 
     return z
 
@@ -25,29 +26,37 @@ np.random.seed(42)
 
 
 def y_func(x):
-    return np.sin(np.pi * x[:, 0]) * np.sin(2 * np.pi * x[:, 1]) - \
-        np.sin(np.pi * x[:, 1]) * np.sin(2 * np.pi * x[:, 0])
+    return np.cos(x)
 
 
-x = np.random.uniform(0, 1, (1000, 2))
+x = np.array([[i] for i in np.linspace(0, 2 * np.pi, 1000)])
+
 
 y = y_func(x)
 
 P = 2 * [None]
 
-P[0] = np.random.normal(0, 1, (3, 10))
-P[0][-1] = 0.01 * np.ones(10)
+P[0] = np.random.normal(0, 1, (2, 20))
+P[0][-1] = 0.01 * np.ones(20)
 
-P[1] = np.random.normal(0, 1, (11, 1))
+print(P[0])
+
+P[1] = np.random.normal(0, 1, (21, 1))
 P[1][-1] = 0.01 * np.ones(1)
 
 
 act_func = [np.tanh, lambda x: x]
 
+feedforward(x, P, act_func)
+
 grad = elementwise_grad(cost, 1)
 
-for i in range(100):
+for i in range(10000):
     print(cost(x, P, act_func, y))
-    gradient = grad(x, P, act_func, y)
-    P[0] -= 0.05 * gradient[0]
-    P[1] -= 0.05 * gradient[1]
+    gradient = grad(x[idx], P, act_func, y[idx])
+    P[0] -= 0.01 * gradient[0]
+    P[1] -= 0.01 * gradient[1]
+
+plt.plot(x, feedforward(x, P, act_func))
+plt.plot(x, y)
+plt.show()
