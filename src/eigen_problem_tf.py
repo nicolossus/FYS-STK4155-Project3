75b3f9212c06a09120dbd9d891ff2f29dd5e3805
Nicolai Haug <nicoha@student.matnat.uio.no>
2019-12-05 05:47:26 +0100

    This seem solid

--- src/eigen_problem_tf.py
+++ src/eigen_problem_tf.py
@@ -1,42 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 import tensorflow as tf
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python.keras import backend as K
 
-from tools import fig_path
-
-tf.keras.backend.set_floatx("float64")
+tf.keras.backend.set_floatx("float32")
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-
-# Set fontsizes in figures
-params = {'legend.fontsize': 'large',
-          'axes.labelsize': 'large',
-          'axes.titlesize': 'large',
-          'xtick.labelsize': 'large',
-          'ytick.labelsize': 'large',
-          'legend.fontsize': 'large',
-          'legend.handlelength': 2}
-plt.rcParams.update(params)
 
 
 class DNModel(tf.keras.Model):
     def __init__(self, n):
         super(DNModel, self).__init__()
 
-        self.dense_1 = tf.keras.layers.Dense(200, activation=tf.nn.sigmoid)
-        self.dense_2 = tf.keras.layers.Dense(100, activation=tf.nn.sigmoid)
+        self.dense_1 = tf.keras.layers.Dense(400, activation=tf.nn.sigmoid)
+        self.dense_2 = tf.keras.layers.Dense(300, activation=tf.nn.relu)
+        self.dense_3 = tf.keras.layers.Dense(200, activation=tf.nn.relu)
+        self.dense_4 = tf.keras.layers.Dense(100, activation=tf.nn.relu)
+        self.dense_5 = tf.keras.layers.Dense(50, activation=tf.nn.sigmoid)
         self.out = tf.keras.layers.Dense(n, name="output")
 
     def call(self, inputs):
         x = self.dense_1(inputs)
         x = self.dense_2(x)
+        x = self.dense_3(x)
+        x = self.dense_4(x)
+        x = self.dense_5(x)
+
         return self.out(x)
-
-
-def ray_quo(A, x):
-    r = tf.matmul(tf.matmul(tf.transpose(x), A), x) / \
-        tf.matmul(tf.transpose(x), x)
-    return r
 
 
 @tf.function
@@ -44,11 +37,12 @@ def trial_solution(model, x0, t):
     """
     Trial solution
     """
-    #gtrial = (1 - t) * x0 + t * model(t)
-    gtrial = tf.exp(-t) * x0 + (1 - tf.exp(-t)) * model(t)
-    #gtrial = tf.exp(-t) * x0 * model(t)
 
-    return tf.transpose(gtrial)
+    gtrial = tf.einsum('i...,j->ij', (1 + t), x0) + \
+        tf.einsum('i...,ij->ij', t, model(t))
+
+    #gtrial = tf.cast(gtrial, tf.float32)
+    return gtrial
 
 
 @tf.function
@@ -56,12 +50,17 @@ def rhs(model, A, x0, t):
     """
     Right-hand side of ODE
     """
+    A = tf.cast(A, tf.float32)
     g = trial_solution(model, x0, t)
-    F1 = tf.matmul(tf.matmul(tf.transpose(g), g) * A, g)
-    F2 = tf.matmul(tf.matmul(tf.transpose(g), A), g) * g
-    rhs_out = F1 - F2
+    # Version 1
+    #F1 = tf.einsum('ij,ij,kl,il->ik', g, g, A, g)
+    #F2 = tf.einsum('ij,jk,ik,il->il', g, A, g, g)
 
-    #rhs_out = tf.matmul(A, g) - tf.matmul(tf.transpose(g), g) * g
+    # Version 2
+    F1 = tf.einsum('jk,ik->ij', A, g)
+    F2 = tf.einsum('ij,ij,ik->ik', g, g, g)
+
+    rhs_out = F1 - F2
 
     return rhs_out
 
@@ -75,10 +74,14 @@ def loss(model, A, x0, t):
     with tf.GradientTape() as tape:
         tape.watch(t)
         trial = trial_solution(model, x0, t)
-        d_trial_dt = tape.gradient(trial, t)
+    d_trial_dt = tape.batch_jacobian(trial, t)
+    d_trial_dt = d_trial_dt[:, :, 0]
 
-    return tf.losses.MSE(
+    #loss_out = tf.losses.MSE(d_trial_dt, rhs(model, A, x0, t))
+    loss_out = tf.losses.MSE(
         tf.zeros_like(d_trial_dt), d_trial_dt - rhs(model, A, x0, t))
+
+    return loss_out
 
 
 '''
@@ -104,15 +107,46 @@ def loss(model, A, x0, t):
     return tf.losses.MSE(tf.zeros_like(g), r * g - tf.matmul(A, g))
 '''
 
-
+# Define gradient method
 @tf.function
 def grad(model, A, x0, t):
     with tf.GradientTape() as tape:
         loss_value = loss(model, A, x0, t)
+
     return loss_value, tape.gradient(loss_value, model.trainable_variables)
 
 
-def euler(A, x0, T, N):
+def ray_quo_tf(A, x):
+    x = x / tf.sqrt(tf.einsum("ij,ij->i", x, x)[:, tf.newaxis])
+    eig = tf.einsum("ij,ij->i", tf.matmul(x, A), x)
+    return eig
+
+
+def ray_quo_np(A, x):
+    x = x / np.sqrt(np.einsum("ij,ij->i", x, x)[:, np.newaxis])
+    eig = np.einsum("ij,ij->i", x@A, x)
+    return eig
+
+
+def f(A, x):
+    return (x.T@x) * A@x - (x.T@A@x) * x
+
+
+def euler(A, x0, t0, t1, n=10001):
+    dt = (t1 - t0) / n
+    x = [x0]
+    for i in range(n - 1):
+        x.append(x[-1] + dt * f(A, x[-1]))
+    x = np.array(x)
+
+    def euler_solution(t):
+        t = t - t0
+        return x[(t * (n - 1)).astype(int), :]
+
+    return euler_solution
+
+
+def euler_eig(A, x0, T, N):
     """
     Euler's method
     """
@@ -133,76 +167,94 @@ def euler(A, x0, T, N):
 if __name__ == "__main__":
     np.random.seed(42)
 
+    n = 3    # Dimension
+    T0 = 0   # Start time
+    T = 20    # Final time
+    N = 151  # number of time points
+
+    # Problem formulation for numpy (Euler and eig solver)
+
+    # Benchmark problem
     n = 3
-    T = 3
-    N = 50
+    T = 1
+    A = np.array([[3., 2., 4.], [2., 0., 2.], [4., 2., 3.]])
+    #A = -A
+    x0 = np.array([1, 0, 0])
+    t = np.linspace(T0, T, N)
+
+    '''
+    # Random problem
 
     A = np.random.normal(0, 1, (n, n))
-    A = (A.T + A) / 2
+    A = (A.T + A) * 0.5
     #A = -A
     x0 = np.random.rand(n)
-    #x0 = x0 / np.linalg.norm(x0, ord=1)
-    #x0 = np.array([1, 0, 0, 0])
-    t = np.linspace(0, T, N)
-    eig_euler = euler(A, x0, T, N)
+    x0 = x0 / np.linalg.norm(x0, ord=1)
+    t = np.linspace(T0, T, N)
+    '''
 
-    A_tf = tf.convert_to_tensor(A, dtype=tf.float64)
-    x0_tf = tf.convert_to_tensor(x0, dtype=tf.float64)
-    t_tf = tf.convert_to_tensor(t, dtype=tf.float64)
+    # Problem formulation for tensorflow
+    A_tf = tf.convert_to_tensor(A, dtype=tf.float32)
+    x0_tf = tf.convert_to_tensor(x0, dtype=tf.float32)
+    t_tf = tf.convert_to_tensor(t, dtype=tf.float32)
+    t_tf = tf.reshape(t_tf, [-1, 1])
 
     # Initial model and optimizer
-
     model = DNModel(n)
-    optimizer = tf.keras.optimizers.Nadam(0.001)
+    optimizer = tf.keras.optimizers.Adam(0.01)
+    num_epochs = 1500
 
-    eigvals_nn = []
+    for epoch in range(num_epochs):
+        cost, gradients = grad(model, A, x0_tf, t_tf)
+        optimizer.apply_gradients(
+            zip(gradients, model.trainable_variables))
 
-    num_epochs = 40
+        step = optimizer.iterations.numpy()
+        if step == 1:
+            print(f"Step: {step}, "
+                  + f"Loss: {tf.math.reduce_mean(cost.numpy())}")
+        if step % 100 == 0:
+            print(f"Step: {step}, "
+                  + f"Loss: {tf.math.reduce_mean(cost.numpy())}")
 
-    for t_ in t:
-        t_ = tf.reshape(t_, [-1, 1])
-        for epoch in range(num_epochs):
+    # Call final models
 
-            cost, gradients = grad(model, A_tf, x0_tf, t_)
-            optimizer.apply_gradients(
-                zip(gradients, model.trainable_variables))
+    # Just for benchmark problem
+    euler_solution = euler(A, x0, T0, T)
+    x_euler = euler_solution(t)
+    #eig_euler = ray_quo_np(A, x_euler)
 
-            print(f"Step: {optimizer.iterations.numpy()}, "
-                  + f"Loss: {tf.reduce_mean(cost.numpy())}")
+    # For all problems
+    eig_euler = euler_eig(A, x0, T, N)
 
-            #g_nn = trial_solution(model, x0_tf, t_)
-            #eig_nn = ray_quo(A_tf, g_nn)
-            # eigvals_nn.append(eig_nn.numpy()[0][0])
+    g = trial_solution(model, x0_tf, t_tf)
+    eig_nn = ray_quo_tf(A_tf, g)
+    eigvals_nn = eig_nn.numpy()
 
-        g_nn = trial_solution(model, x0_tf, t_)
-        eig_nn = ray_quo(A_tf, g_nn)
-        eigvals_nn.append(eig_nn.numpy()[0][0])
-
+    # Print results
     v, w = np.linalg.eig(A)
+    print()
     print('A =', A)
     print('x0 =', x0)
-    print('Eigvals:', v)
+    print('Eigvals Numpy:', v)
+    print('Max Eigval Numpy', np.max(v))
+    print('Final Eigval Euler', eig_euler[-1])
+    print('Final Eigval FFNN', eigvals_nn[-1])
 
-    '''
-    iter_tot = num_epochs * N
-    eig_euler2 = euler(A, x0, T, iter_tot)
-    iter_plot = np.linspace(1, iter_tot, iter_tot)
-    fig, ax = plt.subplots()
-    ax.axhline(np.max(v), color='red', ls='--')
-    ax.plot(iter_plot, eig_euler2)
-    ax.plot(iter_plot, eigvals_nn)
-    ax.set_xlabel('Iteration')
-    ax.set_ylabel('Eigenvalue, $\\lambda$')
-    lgd_numpy = "Numpy $\\lambda_{\\mathrm{max}} \\sim$ " + \
-        str(round(np.max(v), 5))
-    lgd_euler = "Euler $\\lambda_{\\mathrm{max}} \\sim$ " + \
-        str(round(np.max(eig_euler), 5))
-    lgd_nn = "FFNN $\\lambda_{\\mathrm{max}} \\sim$ " + \
-        str(round(max(eigvals_nn), 5))
-    plt.legend([lgd_numpy, lgd_euler, lgd_nn], loc='best')
-    plt.show()
-    '''
+    # Plot results
 
+    # Just for benchmark problem
+    fig0, ax0 = plt.subplots()
+    ax0.plot(t, x_euler[:, 0], ls='--', color='b')
+    ax0.plot(t, x_euler[:, 1], ls='--', color='g')
+    ax0.plot(t, x_euler[:, 2], ls='--', color='r')
+    ax0.plot(t_tf, g[:, 0], color='b')
+    ax0.plot(t_tf, g[:, 1], color='g')
+    ax0.plot(t_tf, g[:, 2], color='r')
+    ax0.set_ylabel('Eigenvector Element Value')
+    ax0.set_xlabel('Time')
+
+    # For all problems
     fig, ax = plt.subplots()
     ax.axhline(np.max(v), color='red', ls='--')
     ax.plot(t, eig_euler)
@@ -211,9 +263,9 @@ if __name__ == "__main__":
     ax.set_ylabel('Eigenvalue, $\\lambda$')
     lgd_numpy = "Numpy $\\lambda_{\\mathrm{max}} \\sim$ " + \
         str(round(np.max(v), 5))
-    lgd_euler = "Euler $\\lambda_{\\mathrm{max}} \\sim$ " + \
-        str(round(np.max(eig_euler), 5))
-    lgd_nn = "FFNN $\\lambda_{\\mathrm{max}} \\sim$ " + \
-        str(round(max(eigvals_nn), 5))
+    lgd_euler = "Euler $\\lambda_{\\mathrm{final}} \\sim$ " + \
+        str(round(eig_euler[-1], 5))
+    lgd_nn = "FFNN $\\lambda_{\\mathrm{final}} \\sim$ " + \
+        str(round(eigvals_nn[-1], 5))
     plt.legend([lgd_numpy, lgd_euler, lgd_nn], loc='best')
     plt.show()
